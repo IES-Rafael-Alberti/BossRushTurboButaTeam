b6fea13d2adc06a450ddd9be48c13cc16c022c76
javijjah <javijjah@gmail.com>
2025-01-09 19:13:51 +0100

    created enemy and started working on attack

--- Scenes/main_char/mc_controller.gd
+++ Scenes/main_char/mc_controller.gd
@@ -1,21 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 600.0
+const JUMP_VELOCITY = -600.0
+
+@onready var attack_area = $AttackArea
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +21,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("attack"):
+		var enemies_in_attack_area = attack_area.get_overlapping_bodies()
+		if enemies_in_attack_area.size()>0:
+			print("attacked")
+		else:
+			print("failed attack")
