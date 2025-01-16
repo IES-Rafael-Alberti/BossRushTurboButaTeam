5b6be549cb8d2530a0d36a055b9fb7d746f2f529
Javier Jiménez-Alfaro Hacha <javijjah@gmail.com>
2025-01-16 16:41:06 +0100

    export category for lion

--- Scenes/lion_boss/lion_boss.gd
+++ Scenes/lion_boss/lion_boss.gd
@@ -1,6 +1,7 @@
 extends Boss
 
 #propiedades del boss, exportadas para editarlas en runtime y más cómodamente
+@export_category("Lion Abilities")
 @export var jump_distance = 300.0
 @export var walk_distance = 75.0
 
