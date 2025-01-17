8207c21b3e5646efdc9c9f809c79e6da9a352946
martoras <yeraymnoz@gmail.com>
2025-01-17 12:26:30 +0100

    fix jump 3

--- Scenes/lion_boss/state_machine/jump.gd
+++ Scenes/lion_boss/state_machine/jump.gd
@@ -13,7 +13,7 @@ func jump():
 func transtions():
 	if owner.is_on_floor():
 		is_jumping = false
-		if owner.global_position.distance_to(owner.player.global_position) > owner.walk_distance:
+		if owner.global_position.distance_to(owner.player.global_position) < owner.walk_distance:
 			state_machine.transition_to("Walk")
 		#else:
 			#state_machine.transition_to("Idle")
@@ -22,7 +22,8 @@ func transtions():
 func update(_delta:float) -> void:
 	if is_jumping:
 		var direction = (owner.player.global_position - owner.global_position).normalized()
-		owner.global_position.x += owner.speed *sign(direction.x)* _delta
+		#owner.global_position.x += owner.speed *sign(direction.x)* _delta
+		owner.global_position.x = owner.player.global_position.x
 	transtions()
 	#owner.global_position += direction * walk_speed * _delta
 
