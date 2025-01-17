4936cfc37ef9393362e4751a5178570162fe5d71
mario <gomezrevillaalicia@gmail.com>
2025-01-17 10:04:59 +0100

    menu gameover y sound

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,18 +1,35 @@
 extends Control
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var play_button: Button = $PlayButton
+@onready var menu_button: Button = $MenuButton
+@onready var credit_button: Button = $CreditButton
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animation_player.play("fade_in")
 
 func _on_play_button_pressed():
-	print("hola")
+	play_button.disabled = true
+	menu_button.disabled = true
+	credit_button.disabled = true
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/cambio_escena.tscn")
 
 func _on_menu_button_pressed():
-	print("puto")
+	play_button.disabled = true
+	menu_button.disabled = true
+	credit_button.disabled = true
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/options.tscn")
 
 func _on_credit_button_pressed():
-	print("eduputo")
+	play_button.disabled = true
+	menu_button.disabled = true
+	credit_button.disabled = true
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
