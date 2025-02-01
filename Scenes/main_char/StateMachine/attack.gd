c31a3f1912717856d1168f71f524bac044201829
SergioHiniesta <sergio2bp@gmail.com>
2025-02-01 19:25:38 +0100

    sounds

--- Scenes/main_char/StateMachine/attack.gd
+++ Scenes/main_char/StateMachine/attack.gd
@@ -1,5 +1,7 @@
 extends State
 
+@onready var attack_sound: AudioStreamPlayer = $"../../AttackSound"
+
 func enter(_msg := {}) -> void:
 	attack()
 	state_machine.transition_to("Idle")
@@ -7,6 +9,7 @@ func enter(_msg := {}) -> void:
 
 func attack():
 	var enemies_in_attack_area = owner.attack_area.get_overlapping_areas()
+	attack_sound.play()
 	if enemies_in_attack_area.size()>0:
 		for enemy in enemies_in_attack_area:
 			if enemy.get_parent().has_method("process_attack"):
